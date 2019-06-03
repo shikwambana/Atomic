586b455c2327e90f29a17e2571bc36fd7ccfafb7
Alphie Shikwambana <shikwambana27@gmail.com>
2019-06-03 12:23:46 +0200

    before change of picture

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -2,10 +2,10 @@ import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 import { appDeclarations, appBootstrap, appProviders, appEntryComponents } from './config/declarations';
 import { appImportModules } from './config/import-modules';
-// import {CountDown} from "ng2-date-countdown";
+import {CountDown} from "ng2-date-countdown";
 
 @NgModule({
-  declarations: [...appDeclarations],
+  declarations: [...appDeclarations, CountDown],
   imports: [...appImportModules],
   providers: [...appProviders],
   entryComponents: [...appEntryComponents],
