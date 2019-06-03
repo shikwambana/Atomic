1f95097966de96dc68d6361ebb467bd81f2af148
Alphie Shikwambana <shikwambana27@gmail.com>
2019-06-03 22:28:53 +0200

    redesigned register page into a card

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { appDeclarations, appBootstrap, appProviders, appEntryComponents } from './config/declarations';
 import { appImportModules } from './config/import-modules';
 import {CountDown} from "ng2-date-countdown";
+// import { Ng2CarouselamosModule } from 'ng2-carouselamos';
 
 @NgModule({
   declarations: [...appDeclarations, CountDown],
