391f0ceb51942a42292c3a0c6dd68cebb6aea023
Alphie Shikwambana <shikwambana27@gmail.com>
2019-06-04 12:14:33 +0200

    improved look of registration page

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -2,12 +2,13 @@ import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 import { appDeclarations, appBootstrap, appProviders, appEntryComponents } from './config/declarations';
 import { appImportModules } from './config/import-modules';
-import {CountDown} from "ng2-date-countdown";
+// import {CountDown} from "ng2-date-countdown";
 // import { Ng2CarouselamosModule } from 'ng2-carouselamos';
+import {SlideshowModule} from 'ng-simple-slideshow';
 
 @NgModule({
-  declarations: [...appDeclarations, CountDown],
-  imports: [...appImportModules],
+  declarations: [...appDeclarations, ],
+  imports: [...appImportModules, SlideshowModule],
   providers: [...appProviders],
   entryComponents: [...appEntryComponents],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
